73918a7af02606775e34920aa77e9d911469cca8
Dale Wijnand <344610+dwijnand@users.noreply.github.com>
2019-01-07 23:33:15 +0000

    Fix relative path issue in scripts/create-forks-play.sh (#77)
    Allows it to be run from any directory (like the root of the repo.)

--- scripts/create-forks-play.sh
+++ scripts/create-forks-play.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-. templates-play.sh
-. lib-fork.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. "$DIR/templates-play.sh"
+. "$DIR/lib-fork.sh"
 
 create-forks $play_github "${play_templates[@]}"
