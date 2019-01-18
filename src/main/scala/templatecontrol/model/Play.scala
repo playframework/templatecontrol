211420f23fe95fb6edf40947774ff5a0bbf245a9
Renato  Cavalcanti <renato@cavalcanti.be>
2019-01-18 16:21:48 +0100

    add hello tutorials

--- src/main/scala/templatecontrol/model/Play.scala
+++ src/main/scala/templatecontrol/model/Play.scala
@@ -5,8 +5,9 @@ object Play {
   private val playOrg = "playframework"
 
   private val templates = Seq(
+
+    Template("play-java-hello-world-tutorial", playOrg),
     Template("play-java-starter-example", playOrg),
-    Template("play-scala-starter-example", playOrg),
     Template("play-java-chatroom-example", playOrg),
     Template("play-java-compile-di-example", playOrg),
     Template("play-java-dagger2-example", playOrg),
@@ -17,6 +18,9 @@ object Play {
     Template("play-java-rest-api-example", playOrg),
     Template("play-java-streaming-example", playOrg),
     Template("play-java-websocket-example", playOrg),
+
+    Template("play-scala-starter-example", playOrg),
+    Template("play-scala-hello-world-tutorial", playOrg),
     Template("play-scala-anorm-example", playOrg),
     Template("play-scala-chatroom-example", playOrg),
     Template("play-scala-compile-di-example", playOrg),
@@ -31,11 +35,11 @@ object Play {
     Template("play-scala-streaming-example", playOrg),
     Template("play-scala-tls-example", playOrg),
     Template("play-scala-websocket-example", playOrg),
-    Template("play-webgoat", playOrg),
+
     Template("play-java-seed.g8", playOrg),
     Template("play-scala-seed.g8", playOrg),
-    Template("play-scala-grpc-example", playOrg),
-    Template("play-java-grpc-example", playOrg),
+
+    Template("play-webgoat", playOrg),
   )
 
   private val templates27Only = Seq(
