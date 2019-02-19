c21ffd640c398a1f8ebc5a0f2bfebaa7d207a6fd
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-02-19 15:43:56 +0100

    Adds missing lagom repos

--- scripts/templates-lagom.sh
+++ scripts/templates-lagom.sh
@@ -7,6 +7,12 @@ declare -a lagom_templates=(
    "lagom-java-maven-chirper-example"
    "lagom-scala.g8"
    "lagom-java.g8"
+   "lagom-scala-openshift-smoketests"
+   "lagom-java-openshift-smoketests"
+   "lagom-scala-grpc-example"
+   "lagom-java-grpc-example"
+   "shopping-cart-scala"
+   "shopping-cart-java"
   )
   
 lagom_github=git@github.com:lagom
