7335815320ab45748cdf285655c3bdc270ab5252
Dale Wijnand <344610+dwijnand@users.noreply.github.com>
2019-03-25 12:40:13 +0000

    Add okhttp, use it for cache and conditional requests! (#137)

--- src/main/scala/templatecontrol/live/LiveGithubClient.scala
+++ src/main/scala/templatecontrol/live/LiveGithubClient.scala
@@ -2,12 +2,20 @@ package templatecontrol.live
 
 import better.files._
 import org.kohsuke.github.GHRepository
+import org.kohsuke.github.extras.OkHttpConnector
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.OkUrlFactory
 import templatecontrol.GitProject
 import templatecontrol.GithubClient
 
 class LiveGithubClient(login: String, oauthToken: String, remote: String, upstream: String)
     extends GithubClient {
-  private val github = org.kohsuke.github.GitHub.connect(login, oauthToken)
+  private val cacheDirectory = new java.io.File("target/github_cache")
+  private val cache = new com.squareup.okhttp.Cache(cacheDirectory, 10 * 1024 * 1024); // 10MB cache
+  private val github = org.kohsuke.github.GitHubBuilder.fromEnvironment()
+      .withOAuthToken(oauthToken, login)
+      .withConnector(new OkHttpConnector(new OkUrlFactory(new OkHttpClient().setCache(cache))))
+      .build()
 
   def clone(workingDir: File, template: String): GitProject = {
     val remoteRepo: GHRepository = github.getRepository(s"$remote/$template")
